caadcab19225ba0b1e06115cc0b7f8b653dbd5ec
Cian <cian.omondi@student.moringaschool.com>
2021-06-18 06:49:01 +0300

    adding user interface and styling

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// user interface logic
+$(document).ready(function (){
+  $('.design-click').click(function(){
+   $('#design-icon').toggle();
+   $('#design-cont').toggle();
+
+  });
+  
+  $().click(function(){
+
+  });
+});
